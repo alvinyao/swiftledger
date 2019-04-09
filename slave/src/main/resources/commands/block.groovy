cc147d79795f1f61040ba315c9300b4bcd888cbf
lichengcai <lichengcai@primeledger.cn>
2019-04-09 16:55:02 +0800

    fix poliytype is null, remove txType at cli

--- slave/src/main/resources/commands/block.groovy
+++ slave/src/main/resources/commands/block.groovy
@@ -1,6 +1,6 @@
 package commands
 
-import com.higgschain.trust.slave.core.repository.PolicyRepository
+import com.higgschain.trust.slave.api.BlockChainService
 import com.higgschain.trust.slave.core.service.block.BlockService
 import com.higgschain.trust.slave.model.bo.Block
 import com.higgschain.trust.slave.model.bo.BlockHeader
@@ -52,27 +52,27 @@ class block {
 
     def printBlock(InvocationContext context, Block block) {
         def header = block.getBlockHeader()
-        printBlockHeader(context, header)
+        printBlockHeader(context, header,block.getSignedTxList().size())
         block.getSignedTxList().forEach({ tx -> printTx(context, tx.getCoreTx()) })
     }
 
-    def printBlockHeader(InvocationContext context, BlockHeader header) {
+    def printBlockHeader(InvocationContext context, BlockHeader header,int number) {
         context.provide(Header: "Height", "": header.height)
         context.provide("Name": "Version", "Value": header.getVersion())
         context.provide("Name": "PreviousHash", "Value": header.getPreviousHash())
         context.provide("Name": "BlockHash", "Value": header.blockHash)
         context.provide("Name": "BlockTime", "Value": DateFormatUtils.format(header.blockTime, "yyyy-MM-dd HH:mm:ss.SSS"))
+        context.provide("Name": "BlockTxNum", "Value": number)
         context.provide("Name": "TotalTxNum", "Value": header.totalTxNum)
         out.println("")
     }
 
     def printTx(InvocationContext context, CoreTransaction ctx) {
         BeanFactory beans = context.attributes['spring.beanfactory']
-        def policyRepository = beans.getBean(PolicyRepository.class)
+        def blockChainService = beans.getBean(BlockChainService.class)
         context.provide("\tTxInfo": "\tTxID", "": ctx.txId)
         context.provide(["Name": "\tPolicyId", "Value": ctx.policyId])
-        context.provide(["Name": "\tPolicyType", "Value": policyRepository.getPolicyType(ctx.getPolicyId())])
-        context.provide(["Name": "\tTxType", "Value": ctx.txType])
+        context.provide(["Name": "\tPolicyType", "Value": blockChainService.getPolicyNameById(ctx.getPolicyId())])
         context.provide(["Name": "\tSender", "Value": ctx.sender])
         context.provide(["Name": "\tSendTime", "Value": DateFormatUtils.format(ctx.sendTime, "yyyy-MM-dd HH:mm:ss.SSS")])
         out.println("")
