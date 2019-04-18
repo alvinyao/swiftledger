50a939c328627bd9855e4465e8bc3a37d6283aa8
lichengcai <lichengcai@primeledger.cn>
2019-04-18 13:56:05 +0800

    fix sh issue

--- bin/deploy-snapshot.sh
+++ bin/deploy-snapshot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-reg='^v(\d+.){1,2}\d+.*&'
-if [[ !"$TRAVIS_BRANCH" =~ $reg ]]; then
+if [[ $TRAVIS_BRANCH == master ]] || \
+   [[ $TRAVIS_BRANCH == dev* ]] || \
+   [[ $TRAVIS_BRANCH == release* ]] ;
+then
   echo -e "Publishing maven snapshot...\n"
   mvn -s bin/settings.xml deploy -Dmaven.install.skip=true -Dmaven.javadoc.skip=true -B
   echo -e "Published maven snapshot"
