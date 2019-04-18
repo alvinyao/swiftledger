9b4fa8e4184371a55bc77e6cbc2516a15ed1be6f
lichengcai <lichengcai@primeledger.cn>
2019-04-18 14:22:45 +0800

    add comment

--- bin/deploy-snapshot.sh
+++ bin/deploy-snapshot.sh
@@ -7,4 +7,6 @@ then
   echo -e "Publishing maven snapshot...\n"
   mvn -s bin/settings.xml deploy -DskipTests -Dmaven.install.skip=true -B
   echo -e "Published maven snapshot"
+else
+  echo -e "branch $TRAVIS_BRANCH no need publish"
 fi
