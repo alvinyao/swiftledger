7e561df70dfaa5148007a8c4be09ccf2550aba23
lichengcai <lichengcai@primeledger.cn>
2019-04-18 14:18:27 +0800

    feature branch not need deploy

--- bin/deploy-snapshot.sh
+++ bin/deploy-snapshot.sh
@@ -2,10 +2,9 @@
 
 if [[ $TRAVIS_BRANCH == master ]] || \
    [[ $TRAVIS_BRANCH == release* ]] || \
-   [[ $TRAVIS_BRANCH == dev* ]] || \
-   [[ $TRAVIS_BRANCH == feature* ]] ;
+   [[ $TRAVIS_BRANCH == dev* ]] ;
 then
   echo -e "Publishing maven snapshot...\n"
-  mvn -s bin/settings.xml deploy -DskipTests -Dmaven.install.skip=true -Dmaven.javadoc.skip=true -B
+  mvn -s bin/settings.xml deploy -DskipTests -Dmaven.install.skip=true -B
   echo -e "Published maven snapshot"
 fi
