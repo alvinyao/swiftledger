6c2e95a86cd5efb85e98d8b273d5616d91c7958f
lichengcai <lichengcai@primeledger.cn>
2019-04-18 10:52:08 +0800

    fix bug

--- bin/deploy-snapshot.sh
+++ bin/deploy-snapshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ !"$TRAVIS_BRANCH" =~  ^v(\d+.){1,2}\d+.*& ]; then
+if [ !"$TRAVIS_BRANCH" =~  `^v(\d+.){1,2}\d+.*&` ]; then
   mvn -s bin/settings.xml deploy -DskipTests -Dmaven.javadoc.skip=true
 fi
