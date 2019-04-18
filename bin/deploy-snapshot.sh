b9b79116c3b9628024d0c5a4c437c16a78470171
lichengcai <lichengcai@primeledger.cn>
2019-04-18 11:22:15 +0800

    fix sh issue

--- bin/deploy-snapshot.sh
+++ bin/deploy-snapshot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-reg = '^v(\d+.){1,2}\d+.*&'
+reg='^v(\d+.){1,2}\d+.*&'
 if [[ !"$TRAVIS_BRANCH" =~ $reg ]]; then
-  mvn -s bin/settings.xml deploy -DskipTests -Dmaven.javadoc.skip=true
+  echo -e "Publishing maven snapshot...\n"
+  mvn -s bin/settings.xml deploy -Dmaven.install.skip=true -Dmaven.javadoc.skip=true -B
+  echo -e "Published maven snapshot"
 fi
